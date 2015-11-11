c2059edef86f839556d8801daee23ca780216c8c
Fernando Moya <fernandoalexander2014@gmail.com>
2015-11-11 01:05:58 -0500

    ejerciciosClase Sesion6

--- bimestre1/ejerciciosClase/ContadorLineas/src/contadorlineas/ContadorLineas.java
+++ bimestre1/ejerciciosClase/ContadorLineas/src/contadorlineas/ContadorLineas.java
@@ -9,44 +9,87 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author 22B
  */
-public class ContadorLineas extends Thread{
+public class ContadorLineas extends Thread {
+
+    private String archivos;
+    private long numeroLineas = 0;
+
+    public ContadorLineas(String archivos) {
+        this.archivos = archivos;
+    }
+
+    public long getNumeroLineas() {
+        return numeroLineas;
+    }
+
+    @Override
+    public void run() {
+        try {
+            FileReader fr1 = new FileReader(this.archivos);
+            BufferedReader bf1 = new BufferedReader(fr1);
+            
+            long contador = 0;
+            String Cadena;
+
+            //sCadena1 = bf1.readLine();
+            while ((Cadena = bf1.readLine()) != null) {
+                //sCadena1 = bf1.readLine();
+                contador++;
+            }
+            this.numeroLineas = contador;
+            
+            //System.out.println(fr1.toString());
+            //System.out.println(contador);
+
+        } catch (FileNotFoundException fnfe) 
+        {
+            Logger.getLogger(ContadorLineas.class.getName()).log(Level.SEVERE, null, fnfe);
+        } catch (IOException fnfe) 
+        {
+            Logger.getLogger(ContadorLineas.class.getName()).log(Level.SEVERE, null, fnfe);
+        }
+    }
 
     public static void main(String[] args) {
 
-        public void run()
-        {
-        try {
-            FileReader fr1 = new FileReader("ABC.txt");
+        ContadorLineas[] vector = new ContadorLineas[args.length];
 
-            BufferedReader bf1 = new BufferedReader(fr1);
-
-            String Cadena;
-            
-            int contador=0;
-            
-
-            //sCadena1 = bf1.readLine();
-
-            while ((Cadena = bf1.readLine()) != null) {
-                //sCadena1 = bf1.readLine();
-                contador ++;
-               
-                
-                
+        for (int i = 0; i < args.length; i++) {
+            if (!args[i].getClass().equals(String.class)) {
+                System.out.println("Archivo en la posicion" + i + ", no es valido");
+                break;
             }
-            System.out.println(fr1.toString());
-             System.out.println(contador);
 
-        } 
-        catch (FileNotFoundException fnfe) {
-            fnfe.printStackTrace();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+            vector[i] = new ContadorLineas(args[i]);
+            vector[i].start();
         }
+
+        int suma = 0;
+        long longitud;
+
+        for (int i = 0; i < vector.length; i++) {
+
+            try {
+                vector[i].join();
+                longitud = vector[i].getNumeroLineas();
+                suma += 1;
+                System.out.println(args[i] + " : " + longitud);
+            } catch (InterruptedException ex) 
+            {
+                Logger.getLogger(ContadorLineas.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
         }
+
+        System.out.println("Total: " + suma);
+
+    }
+
 }
